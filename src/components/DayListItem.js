b3da4a7e62c6a88a8c122a42488c1cf613fc2ca7
sujatha-m <jhanavi.bhushan@gmail.com>
2022-01-05 21:02:42 -0500

    Test cases added to daylistItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,16 +1,31 @@
 import React from "react";
 import "components/DayListItem.scss";
-const classNames = require('classnames')
+const classNames = require('classnames');
+
+const formatSpots = spots => {
+    if (!spots) {
+      return `no spots remaining`;
+    }
+  
+    if (spots === 1) {
+      return `${spots} spot remaining`;
+    }
+  
+    return `${spots} spots remaining`;
+  }
 
 export default function DayListItem(props) {
+    const availabilityMessage = formatSpots(props.spots);
+
     let DayListItemClass = classNames('day-list__item', {
         'day-list__item--selected': props.selected,
         'day-list__item--full': props.spots === 0
      });
+        
   return (
     <li className={DayListItemClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{availabilityMessage}</h3>
     </li>
   );
 }
