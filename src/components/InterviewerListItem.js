8b7d87d7d8d0b12b62b53922870fd62dd7f7e157
sujatha-m <jhanavi.bhushan@gmail.com>
2022-01-06 11:56:24 -0500

    Implemented interviewerList and styles added

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "components/InterviewerListItem.scss";
+const classNames = require('classnames');
+
+export default function InterviewerListItem(props) {
+   
+  let interviewerStyles = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  })
+
+  let imageStyles = classNames("interviewers__item-image", {
+    "interviewers__item--selected-image": props.selected,
+  })
+        
+  return (
+    <li 
+    className={interviewerStyles}
+    onClick={() => props.setInterviewer(props.name)}
+    >
+    <img
+      className={imageStyles}
+      src={props.avatar}
+      alt={props.name}
+    />
+    {props.name}
+  </li>
+  );
+}
