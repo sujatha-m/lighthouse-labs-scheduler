b7a58bd975f52f4eabb6ed322f2bfd65e73987f8
sujatha-m <jhanavi.bhushan@gmail.com>
2022-01-05 21:12:05 -0500

    Implemented DayList component to show days

---
+++ src/components/DayList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+export default function DayList(props){
+    const schedule = props.days.map(dayObj => {
+        return (
+          <DayListItem
+            key={dayObj.id}
+            selected={dayObj.name === props.day}
+            setDay={props.setDay}
+            {...dayObj}
+          />
+        )
+      })
+      return <ul>{schedule}</ul>
+  }
