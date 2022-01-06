6b4b4bd5a5313080d526a7fd895515b4fc1a852b
sujatha-m <jhanavi.bhushan@gmail.com>
2022-01-06 15:30:30 -0500

    refactored and finalized the interviewer component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -5,22 +5,22 @@ import InterviewerListItem from "components/InterviewerListItem";
 const classNames = require("classnames");
 
 export default function InterviewerList(props) {
-  const interviewers = props.interviewers.map((interviewerObj) => {
+  const interviewers = props.interviewers.map(interviewerObj => {
     return (
-      <InterviewerListItem
+      <InterviewerListItem 
         key={interviewerObj.id}
         name={interviewerObj.name}
         avatar={interviewerObj.avatar}
-        selected={interviewerObj.id === props.interviewer}
-        setInterviewer={props.setInterviewer}
+        selected={interviewerObj.id === props.value}
+        setInterviewer={(event) => props.onChange(interviewerObj.id)}
       />
-    );
-  });
+    )
+  })
 
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">{interviewers}</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{interviewers}</ul>
     </section>
   );
 }
