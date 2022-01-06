d15a7ebe0cf19b7ed3beb91a98f37aedc9181ebf
sujatha-m <jhanavi.bhushan@gmail.com>
2022-01-06 12:43:20 -0500

    Refactored interviewer list

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,13 +1,26 @@
 import React from "react";
 import "components/InterviewerList.scss";
-const classNames = require('classnames');
+import InterviewerListItem from "components/InterviewerListItem";
+
+const classNames = require("classnames");
 
 export default function InterviewerList(props) {
-        
+  const interviewers = props.interviewers.map((interviewerObj) => {
+    return (
+      <InterviewerListItem
+        key={interviewerObj.id}
+        name={interviewerObj.name}
+        avatar={interviewerObj.avatar}
+        selected={interviewerObj.id === props.interviewer}
+        setInterviewer={props.setInterviewer}
+      />
+    );
+  });
+
   return (
-  <section className="interviewers">
-  <h4 className="interviewers__header text--light">Interviewer</h4>
-  <ul className="interviewers__list"></ul>
-  </section>
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">{interviewers}</h4>
+      <ul className="interviewers__list"></ul>
+    </section>
   );
 }
