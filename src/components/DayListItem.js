bd01e8e9b6d739d5ca7ea01da41f5eaf1903189c
sujatha-m <jhanavi.bhushan@gmail.com>
2022-02-11 07:57:06 -0500

    debugging tests

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,30 +1,34 @@
 import React from "react";
 import "components/DayListItem.scss";
-const classNames = require('classnames');
+const classNames = require("classnames");
 
-const formatSpots = spots => {
-    if (!spots) {
-      return `no spots remaining`;
-    }
-  
-    if (spots === 1) {
-      return `${spots} spot remaining`;
-    }
-  
-    return `${spots} spots remaining`;
+const formatSpots = (spots) => {
+  if (!spots) {
+    return `no spots remaining`;
   }
 
-export default function DayListItem(props) {
-    const availabilityMessage = formatSpots(props.spots);
+  if (spots === 1) {
+    return `${spots} spot remaining`;
+  }
 
-    let DayListItemClass = classNames('day-list__item', {
-        'day-list__item--selected': props.selected,
-        'day-list__item--full': props.spots === 0
-     });
-        
+  return `${spots} spots remaining`;
+};
+
+export default function DayListItem(props) {
+  const availabilityMessage = formatSpots(props.spots);
+
+  let DayListItemClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  });
+
   return (
-    <li className={DayListItemClass} onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2> 
+    <li
+      className={DayListItemClass}
+      onClick={() => props.setDay(props.name)}
+      data-testid="day"
+    >
+      <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{availabilityMessage}</h3>
     </li>
   );
--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -11,9 +11,9 @@ import {
   wait,
   fireEvent,
   getByPlaceholderText,
+  queryByText
 } from "@testing-library/react";
 import Application from "components/Application";
-import Empty from "components/Appointment/Empty";
 
 afterEach(cleanup);
 
@@ -26,17 +26,31 @@ describe("Application", () => {
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
 
-  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
-    const { container } = render(<Application />);
-    await wait(() => getByText(container, "Archie Cohen"));
-    const appointment = getAllByTestId(container, "appointment")[0];
-
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    const { container, debug } = render(<Application />);
+  
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+  
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+  
     fireEvent.click(getByAltText(appointment, "Add"));
+  
     fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
-      target: { value: "Lydia Miller-Jones" },
+      target: { value: "Lydia Miller-Jones" }
     });
+  
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
     fireEvent.click(getByText(appointment, "Save"));
-    console.log(prettyDOM(appointment));
+  
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+  
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+  
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
+  
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
 });
